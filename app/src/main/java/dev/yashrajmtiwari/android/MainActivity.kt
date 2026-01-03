2a9c4c98c3c526c3f713bd8ea1b2a42b1a71ba6d
YashrajMTiwari <yashrajtiwari4362@gmail.com>
2026-01-03 11:06:46 +0530

    Added README.md

--- app/src/main/java/dev/yashrajmtiwari/android/MainActivity.kt
+++ app/src/main/java/dev/yashrajmtiwari/android/MainActivity.kt
@@ -26,8 +26,32 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SimpleApp() {
     var count by remember { mutableIntStateOf(0) }
+    val context = LocalContext.current
     var name by remember { mutableStateOf("") }
-    CounterUI(count = count, onIncrease = { count++ }, onReset = { count = 0 }, onDecrease = { if (count != 0) {count--} }, onMultiply = { count *= 2})
+    CounterUI(
+        count = count,
+        onIncrease = { count++ },
+        onReset = { count = 0 },
+        onDecrease = {
+            if (count != 0) {
+                count--
+            } else {
+                Toast.makeText(context, "Value cannot be less than 0.", Toast.LENGTH_SHORT).show()
+            } },
+        onMultiply = { if (count != 0) {
+            count *= 2
+        } else {
+            Toast.makeText(context, "Cannot Multiply by zero", Toast.LENGTH_SHORT).show()
+        }
+        }
+    )
+    LaunchedEffect(count) {
+        if (count == 10) {
+            Toast.makeText(context, "Count reached 10!", Toast.LENGTH_SHORT).show()
+        } else if (count == 20) {
+            Toast.makeText(context, "Count reached 20!", Toast.LENGTH_LONG).show()
+        }
+    }
 }
 
 @Preview(showBackground = true)
